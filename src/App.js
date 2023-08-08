022858d591b6fd64b06ba94a293084522c6b55bf
Aveneil Lucena <neillucena97@gmail.com>
2023-08-08 06:10:45 -0700

    Resume and socials buttons at bottom of container

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './index.css';
 
 function App() {
   return (
-    <div className="portfolio-container flex items-center justify-center h-screen bg-gray-000">
-      <div className="portfolio-card-container flex bg-white p-8">
-        <div className="w-/4 pr-6">
-          <div className="w-14 h-24 relative drop-shadow-md shadow-lg shadow-indigo-500/50 gradient-border">
-            <img
-              src={process.env.PUBLIC_URL + '/media/portrait.jpg'}
-              alt="Avatar"
-              className="rounded-full"
-            />
+    <div className="portfolio-container flex flex-col items-center justify-center h-screen bg-gray-000">
+      {/* Portfolio Card Container */}
+      <div className="portfolio-card-container flex p-8 rounded-t-2xl">
+        <div className="w-1/4 pr-6">
+          <div className="flex justify-between items-end h-full">
+            <div className="w-14 h-24 relative drop-shadow-md shadow-lg shadow-indigo-500/50 gradient-border">
+              <img
+                src={process.env.PUBLIC_URL + '/media/portrait.jpg'}
+                alt="Avatar"
+                className="rounded-full"
+              />
+            </div>
           </div>
         </div>
 
@@ -25,21 +28,23 @@ function App() {
           </p>
           {/* You can add more content or components here */}
         </div>
+      </div>
 
-          <div className="flex flex-col-2 justify-between space-y-2">
-              <a
-                href={process.env.PUBLIC_URL + '/media/your-resume.pdf'}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="resume-button flex justify-center font-mono font-bold text-base/10 text-white p-2 w-32 h-14 rounded-full drop-shadow-md  bg-indigo-500
-                hover:bg-indigo-700 "
-              >
-                Resume
-              </a>
-
-            <div className="flex space-between space-x-3">
-                <a
-                  href="https://github.com/aveneil-lucena"
+      {/* Buttons Container */}
+      <div className="portfolio-buttons-container flex justify-between p-4 bg-gray-000 rounded-b-2xl">
+        <a
+          href={process.env.PUBLIC_URL + '/media/your-resume.pdf'}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="resume-button flex justify-center font-mono font-bold text-base/10 text-white p-2 w-32 h-14 
+          rounded-full drop-shadow-md bg-indigo-500 hover:bg-indigo-700 mr-80"
+        >
+          Resume
+        </a>
+        
+        <div className="flex space-x-4 items-center">
+        <a
+                  href="https://github.com/link"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="github-button transition ease-in-out delay-50 w-14 h-14 p-2 bg-gray-200 drop-shadow-md rounded-md 
@@ -52,7 +57,7 @@ function App() {
                   />
                 </a>
                 <a
-                  href="https://www.instagram.com/neil_luc/"
+                  href="https://www.instagram.com/link/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="instagram-button transition ease-in-out delay-50 w-14 h-14 p-2 bg-gray-200 drop-shadow-md rounded-md 
@@ -65,7 +70,7 @@ function App() {
                   />
                 </a>
                 <a
-                  href="https://linkedin.com/in/aveneil-lucena"
+                  href="https://linkedin.com/in/link"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="linkedin-button transition ease-in-out delay-50 w-14 h-14 p-2 bg-gray-200 drop-shadow-md rounded-md 
@@ -77,10 +82,8 @@ function App() {
                     className=""
                   />
                 </a>
-                
-            </div>
-          </div>
-
+          {/* Similar code for other social buttons */}
+        </div>
       </div>
     </div>
   );
