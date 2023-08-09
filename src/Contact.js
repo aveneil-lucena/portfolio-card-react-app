d19d6a1a987453fb8eea629e27515cf71d89f988
Aveneil Lucena <neillucena97@gmail.com>
2023-08-09 04:15:47 -0700

    Calibrated EmailJS for contact form; works!

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 const Contact = ({ closeModal, children }) => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_8epfwtc', 'template_hszdikz', form.current, '3ak7rT509eLo80psg')
+            .then((result) => {
+                console.log(result.text);
+                console.log("Message sent!")
+                // Add any additional logic here, such as showing a success message
+            }, (error) => {
+                console.log(error.text);
+                console.log("There was an error sending the message.")
+                // Handle the error and provide user feedback if needed
+        });
+    };
+
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
@@ -27,30 +45,25 @@ const Contact = ({ closeModal, children }) => {
                     </p>
                 </div>
                 <div className="mt-12 mx-auto px-24 p-8 bg-white sm:max-w-lg sm:px-8 sm:rounded-xl">
-                    <form
-                        onSubmit={(e) => e.preventDefault()}
-                        className="space-y-5"
-                    >
-                        <div>
-                            <label className="font-medium">
-                                Full Name
-                            </label>
-                            <input
-                                type="text"
-                                required
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
-                            />
-                        </div>
-                        <div>
-                            <label className="font-medium">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                required
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
-                            />
-                        </div>
+                <form ref={form} onSubmit={sendEmail} className="space-y-5">
+  <div>
+    <label className="font-medium">Full Name</label>
+    <input
+      type="text"
+      name="user_name"
+      required
+      className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+    />
+  </div>
+  <div>
+    <label className="font-medium">Email</label>
+    <input
+      type="email"
+      name="user_email"
+      required
+      className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+    />
+  </div>
                         {/*<div>
                             <label className="font-medium">
                               Phone Number
@@ -71,17 +84,21 @@ const Contact = ({ closeModal, children }) => {
                                 />
                             </div>
                         </div>*/}
-                        <div>
-                            <label className="font-medium">
-                              Message
-                            </label>
-                            <textarea required className="w-full mt-2 h-56 px-3 py-1 resize-none appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg" placeholder="..."></textarea>
-                        </div>
-                        <button
-                            className="w-full px-2 py-2 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-lg duration-150"
-                        >
-                            Submit
-                        </button>
+  <div>
+    <label className="font-medium">Message</label>
+    <textarea
+      name="message"
+      required
+      className="w-full mt-2 h-56 px-3 py-1 resize-none appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+      placeholder="..."
+    ></textarea>
+  </div>
+  <button
+    type="submit"
+    className="w-full px-2 py-2 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-lg duration-150"
+  >
+    Submit
+  </button>
                     </form>
                 </div>
             </div>
