a49b9c821cf8773f09bb3768639c84366b285f32
Aveneil Lucena <neillucena97@gmail.com>
2023-08-09 03:58:54 -0700

    Installed EmailJS

--- src/Contact.js
+++ src/Contact.js
@@ -51,7 +51,7 @@ const Contact = ({ closeModal, children }) => {
                                 className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
                             />
                         </div>
-                        <div>
+                        {/*<div>
                             <label className="font-medium">
                               Phone Number
                             </label>
@@ -70,7 +70,7 @@ const Contact = ({ closeModal, children }) => {
                                     className="w-full pl-[4.5rem] pr-3 py-2 appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
                                 />
                             </div>
-                        </div>
+                        </div>*/}
                         <div>
                             <label className="font-medium">
                               Message
