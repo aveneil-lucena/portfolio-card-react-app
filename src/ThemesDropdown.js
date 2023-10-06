23bc5a2e00f903fa67a622d03e56ff7913ef752c
Aveneil Lucena <neillucena97@gmail.com>
2023-10-06 07:44:46 -0700

    Revamped the themes dropdown: now is three buttons with hover description, and custom icons

--- src/ThemesDropdown.js
+++ src/ThemesDropdown.js
@@ -1,25 +1,41 @@
 import React from 'react';
 
 const ThemesDropdown = ({ currentTheme, onChange }) => {
-  const themes = [
-    { value: 'light', label: 'Light' },
-    { value: 'dark', label: 'Dark' },
-    { value: 'nebula', label: 'Nebula' },
-  ];
-
   return (
     <div className={`themes-button ${currentTheme} flex absolute top-0 left-0 z-10`}>
-      <select
-        onChange={(e) => onChange(e.target.value)}
-        className="rounded-lg px-4 py-2 bg-gray-100 dark:bg-gray-100 shadow-lg"
-        value={currentTheme} // Sets the current selected value
+      <button
+        className={`theme-button w-12 h-12 p-2 m-1 bg-gray-300 drop-shadow-lg rounded-3xl 
+        bg-gradient-to-t from-orange-100 to-transparent ${currentTheme === 'light' ? 'active' : ''}`}
+        onClick={() => onChange('light')}
+        title="Click to enable the Light theme"
       >
-        {themes.map((theme) => (
-          <option key={theme.value} value={theme.value}>
-            {theme.label}
-          </option>
-        ))}
-      </select>
+        <img
+          src={process.env.PUBLIC_URL + '/icons/sun-icon.svg'} // Replace with your SVG file path
+          alt="Sun"
+        />
+      </button>
+      <button
+        className={`theme-button w-12 h-12 p-2 m-1 bg-gray-300 drop-shadow-lg rounded-3xl 
+        bg-gradient-to-t from-red-200 to-transparent ${currentTheme === 'dark' ? 'active' : ''}`}
+        onClick={() => onChange('dark')}           
+        title="Click to enable the Dark theme"
+      >
+        <img
+          src={process.env.PUBLIC_URL + '/icons/moon-icon.svg'} // Replace with your SVG file path
+          alt="Moon"
+        />
+      </button>
+      <button
+        className={`theme-button w-12 h-12 p-2 m-1 bg-gray-300 drop-shadow-lg rounded-3xl 
+        bg-gradient-to-t from-purple-300 to-transparent ${currentTheme === 'nebula' ? 'active' : ''}`}
+        onClick={() => onChange('nebula')}
+        title="Click to enable the Nebula theme"
+      >
+        <img
+          src={process.env.PUBLIC_URL + '/icons/stars-icon.svg'} // Replace with your SVG file path
+          alt="Planet"
+        />
+      </button>
     </div>
   );
 };
